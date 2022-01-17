19920ea7fdcd466f23959358cec2c24328ba1a79
Jonathan McGaha <BsMWgErTYR4LEDR9@protonmail.ch>
2022-01-17 01:11:43 -0500

    css grid adjustments

--- Components/like_button.js
+++ Components/like_button.js
@@ -10,7 +10,7 @@ class LikeButton extends React.Component {
 
     render() {
         if (this.state.liked) {
-            return 'You liked this.';
+            return 'You liked comment number ' + this.props.commentID;
         }
 
         return e(
@@ -20,8 +20,14 @@ class LikeButton extends React.Component {
         );
     }
 }
-// ... the starter code you pasted ...
 
-const domContainer =
-    document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+    .forEach(domContainer => {
+        // Read the comment ID from a data-* attribute.
+        const commentID = parseInt(domContainer.dataset.commentid, 10);
+        ReactDOM.render(
+            e(LikeButton, { commentID: commentID }),
+            domContainer
+        );
+    });
